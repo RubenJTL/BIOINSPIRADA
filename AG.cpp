40cd6f6f1bcf0c49ded878ce23c4b688aaf0e774
Ruben Torres Lima <ruben_jtl@hotmail.com>
2018-07-16 11:44:25 -0500

    Add files via upload

---
+++ AG.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdlib.h>   //atoi
+#include <math.h>
+#include <cstdlib>
+#include <ctime>
+#include "AG.h"
+
+using namespace std;
+
+int main(){
+	srand(time(NULL));
+	//string iterator;
+    AG a(1);
+   // while(true){
+   	//cout<<endl;
+   	//a.run();
+   	
+		    
+
+
+    return 0;
+}
